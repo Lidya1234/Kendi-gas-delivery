6fef0a5688f9ba3ceb6838cb44c5e3b5c05506e0
Lidya Ghebreigziabher <lghebreigziabher@kabbee.org>
2022-06-16 19:24:11 +0300

    Add components

---
+++ src/components/Mission.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Mission() {
+  return (
+    
+    // <aside class="text-center bg-gradient-primary-to-secondary">
+       <aside class="text-center  bg-gradient-primary-to-secondary">
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-xl-8">
+                <div class="h2 fs-1 text-white mb-4">"Our mission is to promote sustainability and improve lives  by creating practical and  useful  systems, to enable the average African have better standard of living."</div>
+                {/* <img src="assets/img/tnw-logo.svg" alt="..." style={{ height: '3rem' }} /> */}
+
+            </div>
+        </div>
+    </div>
+</aside>
+  )
+}
